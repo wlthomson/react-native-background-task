0663967815434720639e98a22618bb7e66ea1e59
wlthomson <will.tho@outlook.com>
2021-05-24 16:25:34 +1200

    Add headless logic

--- App.js
+++ App.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { Image, StyleSheet } from 'react-native';
+import { Image, StyleSheet, NativeModules } from 'react-native';
+
+const { AlarmManagerModule } = NativeModules;
 
 const App = () => {
+  React.useEffect(() => {
+    AlarmManagerModule.setRepeating('android.intent.action.taskservicereceiver', 60000);
+  }, [])
+
   const uri = useSelector(store => store.url);
   return <Image style={styles.image} source={{ uri }}/>
 };
--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import { Provider } from 'react-redux';
 
 import App from './App';
 
-import { name as appName } from './app.json';
+import { name as appName, taskName } from './app.json';
 
 const reducer = (state = { url: "https://cdn2.thecatapi.com/images/e11.jpg" }, action) => {
     switch (action.type) {
@@ -25,4 +25,12 @@ const updateUrl = url => ({
 
 const Root = () => <Provider store={store}><App/></Provider>;
 
+const Task = async (taskData) => {
+    fetch(taskData.url).then(response => response.json()).then(json => {
+        const [{ url }] = json;
+        store.dispatch(updateUrl(url));
+    } )
+};
+
 AppRegistry.registerComponent(appName, () => Root);
+AppRegistry.registerHeadlessTask(taskName, () => Task);
